ebb8cc5b08762e3d7ae1cc7cdbac86d82144fd7d
SWASTIK CHATTERJEE <chatterjeeswastik7772@gmail.com>
2024-01-29 10:30:35 +0530

    first commit

---
+++ src/pages/components/VideoCard.tsx
@@ -0,0 +1,27 @@
+
+export function VideoCard(){
+    return(
+        
+        <div>
+            <img src="image1.png"></img>
+            <div className="grid grid-cols-12">
+                <div className="col-span-1">
+                    <img src="image2.jpg"></img>
+                </div>
+                <div className="col-span-11">
+                    <div >
+                        Hello Apple Vision Pro
+                    </div>
+                    <div >
+                        Apple
+                    </div>
+                    <div >
+                        202K views . 5 hours ago
+                    </div>
+                </div>
+            </div>
+        </div>
+            
+        
+    )
+}
