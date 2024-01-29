49eadacf9faffe399bf723b010a78d0a55b7cffe
SWASTIK CHATTERJEE <chatterjeeswastik7772@gmail.com>
2024-01-29 10:56:10 +0530

    2nd commit

--- src/pages/components/VideoCard.tsx
+++ src/pages/components/VideoCard.tsx
@@ -3,21 +3,17 @@ export function VideoCard(){
     return(
         
         <div>
-            <img src="image1.png"></img>
-            <div className="grid grid-cols-12">
+            <img className="rounded-xl" src="image1.jpg"></img>
+            <div className="grid grid-cols-12 pt-2">
+
                 <div className="col-span-1">
-                    <img src="image2.jpg"></img>
+                    <img className={"rounded-full w-12 h-12"} src="image2.jpg"></img>
                 </div>
-                <div className="col-span-11">
-                    <div >
-                        Hello Apple Vision Pro
-                    </div>
-                    <div >
-                        Apple
-                    </div>
-                    <div >
-                        202K views . 5 hours ago
-                    </div>
+
+                <div className="col-span-11 pl-2">
+                    <div >Hello Apple Vision Pro</div>
+                    <div className="col-span-11 text-gray-400 text-sm">Apple</div>
+                    <div className="col-span-11 text-gray-400 text-sm">202K views . 5 hours ago</div>
                 </div>
             </div>
         </div>
